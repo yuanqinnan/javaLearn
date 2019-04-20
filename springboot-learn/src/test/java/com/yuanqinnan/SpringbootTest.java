fae8b413f9bf793e0f53138e452295a5ba38ef04
Anan <1546022688@qq.com>
2019-04-20 22:22:38 +0800

    clear code

---
+++ springboot-learn/src/test/java/com/yuanqinnan/SpringbootTest.java
@@ -0,0 +1,31 @@
+package com.yuanqinnan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * @author yuanqn
+ * @date 2019/4/1921:00
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void test() throws SQLException {
+        System.out.println(dataSource.getClass());
+        Connection connection = dataSource.getConnection();
+
+
+    }
+}
