59c0fb5e16580998b91455eb58fc3f598ac9af92
yuanqinnan <1546022688@qq.com>
2019-07-03 16:03:16 +0800

    多线程基础学习(三)

---
+++ base/src/main/java/com/yuanqinnan/api/synchronizeds/SynchronizedDemo1.java
@@ -0,0 +1,46 @@
+package com.yuanqinnan.api.synchronizeds;
+
+/**
+ * @author:yuanqinnan
+ * @date: 2019/7/3 9:41
+ */
+
+
+public class SynchronizedDemo1 {
+
+    private int count;
+
+    public synchronized void countAdd() {
+        for (int i = 0; i < 5; i++) {
+            try {
+                System.out.println(Thread.currentThread().getName() + ":" + (count++));
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void print() {
+        System.out.println(Thread.currentThread().getName() + "的打印方法:" + count);
+    }
+
+    public static void main(String[] args) {
+        SynchronizedDemo1 demo1 = new SynchronizedDemo1();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                demo1.countAdd();
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                demo1.print();
+                demo1.countAdd();
+
+            }
+        }).start();
+    }
+}
---
+++ base/src/main/java/com/yuanqinnan/api/synchronizeds/SynchronizedDemo3.java
@@ -0,0 +1,44 @@
+package com.yuanqinnan.api.synchronizeds;
+
+
+/**
+ * @author:yuanqinnan
+ * @date: 2019/7/3 11:25
+ */
+
+public class SynchronizedDemo3 {
+
+    private int count;
+
+    public void countAdd() {
+        synchronized (SynchronizedDemo3.class) {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    System.out.println(Thread.currentThread().getName() + ":" + (count++));
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SynchronizedDemo3 demo1 = new SynchronizedDemo3();
+        SynchronizedDemo3 demo3 = new SynchronizedDemo3();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                demo1.countAdd();
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                demo3.countAdd();
+
+            }
+        }).start();
+    }
+}
