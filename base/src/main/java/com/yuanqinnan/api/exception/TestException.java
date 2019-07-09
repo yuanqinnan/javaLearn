0c736dcb2ed6b103e34e367c5567f72d027b8af9
yuanqinnan <1546022688@qq.com>
2019-07-09 19:54:13 +0800

    多线程基础学习(四)

---
+++ base/src/main/java/com/yuanqinnan/api/exception/TestException.java
@@ -0,0 +1,25 @@
+package com.yuanqinnan.api.exception;
+
+/**
+ * @author:yuanqinnan
+ * @date: 2019/7/9 14:31
+ */
+
+public class TestException {
+
+    public static void main(String[] args) {
+        String msg = test();
+        System.out.println(msg);
+    }
+
+    public static String test() {
+        try {
+            return "try中的返回值";
+        } catch (Exception ex) {
+            return "catch中的返回值";
+        } finally {
+            return "finally的返回值";
+        }
+
+    }
+}
